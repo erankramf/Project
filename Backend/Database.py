50d0bdaa86ed44062447f2b26d82de4660647c71
erankramf <eran.kramf@mail.huji.ac.il>
2023-12-09 18:55:14 +0200

    Managed to connect to our mongodb. The ping_server function tries to ping the db and retuns success or failure reason

--- Backend/Database.py
+++ Backend/Database.py
@@ -1,7 +1,31 @@
-import motor.motor_asyncio
 
 #Just copied from example Github, changed the url. Not sure if works. 
 #Anyone can change when doing the connect to db ticket
-client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://read:XgFXpjCQZznKddf4KvtW@cta-simpipe-protodb.zeuthen.desy.de/?authMechanism=DEFAULT&authSource=admin&tls=true')
+# client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://read:XgFXpjCQZznKddf4KvtW@cta-simpipe-protodb.zeuthen.desy.de/?authMechanism=DEFAULT&authSource=admin&tls=true')
+
 #database = client.TodoList
 #collection = database.todo
+
+
+import asyncio
+from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo.server_api import ServerApi
+# Replace the placeholder with your Atlas connection string
+uri = "mongodb://read:XgFXpjCQZznKddf4KvtW@cta-simpipe-protodb.zeuthen.desy.de/?authMechanism=DEFAULT&authSource=admin&tls=true"
+# Set the Stable API version when creating a new client
+client = AsyncIOMotorClient(uri, server_api=ServerApi('1'))
+db = client["CTA-Simulation-Model"]
+telescopes_collection = db["telescopes"]
+async def ping_server():
+  # Send a ping to confirm a successful connection
+  try:
+    await client["CTA-Simulation-Model"].command('ping')  
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+    return "Pinged your deployment. You successfully connected to MongoDB!"
+  except Exception as e:
+    print(e)
+    return str(e)
+      
+
+async def print_client():
+    return await ping_server()
