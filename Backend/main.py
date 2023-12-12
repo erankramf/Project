b55c664bdcb6b7f2eb2676a079991eb7357af9f3
Selin Orhanlar <140173805+selinorhanlar@users.noreply.github.com>
2023-12-12 21:36:45 +0000

    created first functions: getParamsByName, getTelescopesByName

--- Backend/main.py
+++ Backend/main.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 from Database import print_client
+
+from Service import(
+    getParamsByName,
+    getTelescopeByName,
+)
 
 app = FastAPI()
 
@@ -20,6 +25,26 @@ app.add_middleware(
 async def getter():
     response = await print_client()
     return response
+
+#do we want all the parameters?
+@app.get("getData/Telescopes/TelescopeName") #we can change how this path looks depending on the things we will need to get done
+async def getParamsByName(TelescopeName):
+    response = await getParamsByName(TelescopeName) #Service-Call
+    if response:
+        return response
+    raise HTTPException(404, f"couldn't find Telescope Parameters")
+
+#what does getting the telescope do? ByName because maybe we will also want to get them by ID
+@app.get("/getData/Telescopes/{TelescopeName}")
+async def getTelescopeByName(TelescopeName):
+    response = await getTelescopeByName(TelescopeName)
+    if response:
+        return response
+    raise HTTPException(404, f"couldn't find Telescope")
+
+#maybe a "getTelescopes" function for displaying all the Telescopes that are in the Database at the moment
+#I don't understand the getTelescope() one, what do we get there?
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
