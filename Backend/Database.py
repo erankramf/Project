7000f4a86cc337330f05eb3341346742d9912d6b
Ani Bucen-Berlinski <bucenani@hu-berlin.de>
2023-12-13 14:34:47 +0100

    funtions for Telescopes and Params

--- Backend/Database.py
+++ Backend/Database.py
@@ -2,7 +2,9 @@ import asyncio
 from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo.server_api import ServerApi
 
+
 uri = "mongodb://read:XgFXpjCQZznKddf4KvtW@cta-simpipe-protodb.zeuthen.desy.de/?authMechanism=DEFAULT&authSource=admin&tls=true"
+#uri = "mongodb://localhost:27017" #local for testing
 
 client = AsyncIOMotorClient(uri, server_api=ServerApi('1'))
 # Not quite sure these are what we need, but we can work with that and maybe change later.
@@ -22,3 +24,20 @@ asyncio.run(ping_server())
 
 async def print_client():
     return await ping_server()
+  
+async def getParamsByName(TelName):
+  cursor = telescopes_collection.find({"Telescope":TelName})
+  Params = []
+  async for telescope in cursor:
+    if telescope not in Params:
+      Params.append(telescope.get("Parameters"))
+  return Params
+  
+  
+async def getTelescopes():
+  TelescopeList = []
+  cursor = await telescopes_collection.find()
+  async for tel in cursor:
+    if tel.get("Telescope") not in TelescopeList:
+      TelescopeList.append(tel.get("Telescope"))
+  return TelescopeList
